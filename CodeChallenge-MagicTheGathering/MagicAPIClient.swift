43b6355dbf47be543e8e650a9037062f757676f4
bmanjuu <bettyofung@gmail.com>
2017-02-01 18:07:29 -0500

    added api client

---
+++ CodeChallenge-MagicTheGathering/MagicAPIClient.swift
@@ -0,0 +1,17 @@
+//
+//  MagicAPIClient.swift
+//  CodeChallenge-MagicTheGathering
+//
+//  Created by Betty Fung on 2/1/17.
+//  Copyright © 2017 Betty Fung. All rights reserved.
+//
+
+import Foundation
+
+struct MagicAGPIClient {
+    
+    let getCardsBaseURL = "https://api.magicthegathering.io/v1/cards"
+    // var searchRequestURL = getCardsBaseURL + user query paramters
+    
+    
+}
